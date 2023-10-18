1c9be617c8068a38cdcb857f9c921b7eac47cdb5
Szer13 <shad350@gmail.com>
2023-10-18 16:15:11 +0200

    added binding to categories form

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -3,6 +3,7 @@ package pl.coderslab.charity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.coderslab.charity.model.Donation;
 import pl.coderslab.charity.repository.CategoryRepository;
 
 @Controller
@@ -16,6 +17,7 @@ public class DonationController {
 
     @GetMapping("/donation")
     public String displayDonationForm(Model model){
+        model.addAttribute("donation", new Donation());
         model.addAttribute("categories", categoryRepository.findAll());
         return "app/form";
     }
