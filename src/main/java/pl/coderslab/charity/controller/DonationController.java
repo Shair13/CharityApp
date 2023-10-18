8dd6c4c7e7455482c74d8047210135f6d9917363
Szer13 <shad350@gmail.com>
2023-10-18 16:26:21 +0200

    form step 3

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -5,20 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.coderslab.charity.model.Donation;
 import pl.coderslab.charity.repository.CategoryRepository;
+import pl.coderslab.charity.repository.InstitutionRepository;
 
 @Controller
 public class DonationController {
 
     private final CategoryRepository categoryRepository;
+    private final InstitutionRepository institutionRepository;
 
-    public DonationController(CategoryRepository categoryRepository) {
+    public DonationController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository) {
         this.categoryRepository = categoryRepository;
+        this.institutionRepository = institutionRepository;
     }
 
     @GetMapping("/donation")
     public String displayDonationForm(Model model){
         model.addAttribute("donation", new Donation());
         model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("institutions", institutionRepository.findAll());
         return "app/form";
     }
 
