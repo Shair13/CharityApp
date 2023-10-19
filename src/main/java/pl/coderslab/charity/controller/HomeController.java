bf46c11ecde2c0d521a57f8dcac8a28dcf4a6c40
Szer13 <shad350@gmail.com>
2023-10-19 21:54:56 +0200

    registration

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -2,9 +2,12 @@ package pl.coderslab.charity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.charity.model.User;
 import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
+import pl.coderslab.charity.repository.UserRepository;
 
 
 @Controller
@@ -12,18 +15,34 @@ public class HomeController {
 
     private final InstitutionRepository institutionRepository;
     private final DonationRepository donationRepository;
+    private final UserRepository userRepository;
 
-    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository, UserRepository userRepository) {
         this.institutionRepository = institutionRepository;
         this.donationRepository = donationRepository;
+        this.userRepository = userRepository;
     }
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String homeAction(Model model){
         model.addAttribute("institutions", institutionRepository.findAll());
         model.addAttribute("sumQuantity", donationRepository.sumQuantity());
         model.addAttribute("donations", donationRepository.findAll().size());
         return "home/home";
+    }
 
+    @GetMapping("/registration")
+    public String displayRegistrationForm(Model model){
+        model.addAttribute("user", new User());
+        return "home/registration-form";
+    }
+
+    @PostMapping("/registration")
+    public String processRegistrationForm(User user, BindingResult result, @RequestParam String password2){
+     if(result.hasErrors() || !user.getPassword().equals(password2)){
+         return "home/registration-form";
+     }
+        userRepository.save(user);
+        return "redirect:/";
     }
 }
