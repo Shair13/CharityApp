57f1ba52bbb51d790b30b91a4db1a24c68676891
Szer13 <shad350@gmail.com>
2023-11-15 17:32:39 +0100

    donation confirmation, contact form

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -1,6 +1,7 @@
 package pl.coderslab.charity.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -43,7 +44,7 @@ public class HomeController {
     }
 
     @PostMapping("/registration")
-    public String processRegistrationForm(User user, BindingResult result, @RequestParam String password2) {
+    public String processRegistrationForm(User user, BindingResult result, @RequestParam String password2, Model model) {
         if (result.hasErrors() || !user.getPassword().equals(password2)) {
             return "home/registration-form";
         }
@@ -53,13 +54,17 @@ public class HomeController {
         String link = "http://localhost:8080/activation/" + user.getUuid();
         String emailText = "Cześć " + user.getFirstName() + ", oto link aktywacyjny do Twojego konta: " + link + " - kliknij, aby aktywować konto i móc się zalogować.";
         emailService.sendSimpleMessage(user.getEmail(), "Account confirmation", emailText);
-        return "redirect:/";
+        String message = "Udało sie! Zajrzyj na maila, tam znajdziesz link aktywacyjny do Twojego konta.";
+        model.addAttribute("message", message);
+        return "home/success-page";
     }
 
     @GetMapping("/activation/{uuid}")
-    public String accountActivation(@PathVariable UUID uuid) {
+    public String accountActivation(@PathVariable UUID uuid, Model model) {
         userOperationService.activateUser(uuid);
-        return "redirect:/";
+        String message = "Konto zostało aktywowane! Teraz możesz się zalogować :)";
+        model.addAttribute("message", message);
+        return "home/success-page";
     }
 
     @GetMapping("/reminder")
@@ -68,7 +73,7 @@ public class HomeController {
     }
 
     @PostMapping("/reminder")
-    public String passwordReminder(@RequestParam String email) {
+    public String passwordReminder(@RequestParam String email, Model model) {
         User user = userService.findByEmail(email);
         if (user != null) {
             UUID uuid = UUID.randomUUID();
@@ -77,7 +82,9 @@ public class HomeController {
             String link = "http://localhost:8080/newpass/" + user.getUuid();
             String emailText = "Cześć " + user.getFirstName() + ", oto link do zresetowania hasła: " + link + " - kliknij, aby przejść do formularza i ustawić nowe hasło.";
             emailService.sendSimpleMessage(email, "Reset password", emailText);
-            return "redirect:/";
+            String message = "Udało sie! Zajrzyj na maila, tam znajdziesz link do formularza zmiany hasła.";
+            model.addAttribute("message", message);
+            return "home/success-page";
         }
         return "error/user-not-found";
     }
@@ -90,8 +97,19 @@ public class HomeController {
     }
 
     @PostMapping("/newpass")
-    private String processNewPasswordForm(UserDTO userDTO, @RequestParam String password2) {
+    public String processNewPasswordForm(UserDTO userDTO, @RequestParam String password2, Model model) {
         userOperationService.updatePassword(userDTO, password2);
-        return "redirect:/";
+        String message = "Hasło zostało zmienione! :)";
+        model.addAttribute("message", message);
+        return "home/success-page";
+    }
+
+    @PostMapping("/contact")
+    public String contactMessage(@RequestParam String name, @RequestParam String surname, @RequestParam String message, Model model) {
+        String emailText = "Message from " + name + " " + surname + ": " + message;
+        emailService.sendSimpleMessage("charityapp2024@gmail.com", "Message from customer", emailText);
+        String succesMessage = "Wiadomość została wysłana. Odpowiemy najszybciej jak to tylko możliwe.";
+        model.addAttribute("message", succesMessage);
+        return "home/success-page";
     }
 }
