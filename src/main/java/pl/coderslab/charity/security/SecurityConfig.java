cdcb29f65433023d88e665784873a97eec4272b9
Szer13 <shad350@gmail.com>
2024-07-19 23:32:17 +0200

    added safe address to securityFilterChain and added docker compose file

--- src/main/java/pl/coderslab/charity/security/SecurityConfig.java
+++ src/main/java/pl/coderslab/charity/security/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(r -> r
-                        .requestMatchers("/", "/registration", "/resources/**", "/activation/**", "/reminder/**", "/forbidden", "/newpass/**", "/contact").permitAll()
+                        .requestMatchers("/", "/registration", "/resources/**", "/activation/**", "/reminder/**", "/forbidden", "/newpass/**", "/contact", "/sqlinit").permitAll()
                         .requestMatchers("/donation", "/profile/**", "/confirmation").authenticated()
                         .requestMatchers("/dashboard/**").hasRole("ADMIN"))
                 .formLogin(f -> f.loginPage("/login").usernameParameter("email").defaultSuccessUrl("/").permitAll())
